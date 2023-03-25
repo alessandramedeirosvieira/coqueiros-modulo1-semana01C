44bc394e8cecb513f5daee0b9fce5515db74289f
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-03-25 20:57:14 -0300

    add exer 9

--- EXERCICIO9/Program.cs
+++ EXERCICIO9/Program.cs
@@ -1,45 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine ("Bem vindo ao Banco Nacional Coqueiros, por favor selecione uma opção para continuar:");
-Console.WriteLine ("1 -Adicionar Transação;");
-Console.WriteLine ("2 -Consultar extrato;");
-Console.WriteLine ("3 -Sair;");
-Console.Write ("Digite a opção escolhida: ");
+string selecao;
+ double saldo=0;
+ 
+ //loop MENU:
+ do{ 
+   Console.WriteLine("Bem vindo ao Banco Nacional Coqueiros, por favor digite a opção desejada: ");
+      Console.WriteLine("1-Adicionar transação");
+      Console.WriteLine("2-Consultar extrato");
+      Console.WriteLine("3-Sair");
+       selecao=Console.ReadLine();
+       Console.WriteLine("Você selecionou "+selecao);
+ }while (!(selecao=="1" || selecao=="2" || selecao=="3"));
 
-int opcao = int.Parse (Console.ReadLine());
-
-if (opcao == 1)
-{
-    Console.WriteLine ("Adicionar Transação");
-}
-else if (opcao == 2) 
-{
-    Console.WriteLine ("Consultar extrato");
-}
-else if (opcao == 3)
-{
-    Console.WriteLine ("Sair");
-}
-else
-{
-    Console.WriteLine ("Escolha a opção desejada: 1 - Adicionar Transação; 2 - Consultar extrato; 3 - Sair;");
-}
+ List <double> conta= new List<double>();
+ double deposito;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Money deposit to account:
+   if (selecao=="1") {
+      Console.WriteLine("Digite abaixo o valor que deseja depositar:");
+      deposito=double.Parse(Console.ReadLine());
+      conta.Add(deposito);
+      } 
+      
+      //Account balance:
+      else if (selecao== "2"){
+      
+      for (int i=0; i < (conta.Count); i++){
+          Console.WriteLine("Depósito de R$:"+conta[i]);
+          saldo=saldo+conta[i];
+      }
+      Console.WriteLine("Saldo atual= R$"+saldo);
+ }
+ //Leaving the bank system:
+ else{
+   Console.WriteLine("Muito obrigado por utulizar nosso sistema, até a próxima.");
+ }
--- EXERCICIO9/obj/Debug/net7.0/EXERCICIO9.AssemblyInfo.cs
+++ EXERCICIO9/obj/Debug/net7.0/EXERCICIO9.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("exercicio9")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EXERCICIO9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("exercicio9")]
-[assembly: System.Reflection.AssemblyTitleAttribute("exercicio9")]
+[assembly: System.Reflection.AssemblyProductAttribute("EXERCICIO9")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EXERCICIO9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
